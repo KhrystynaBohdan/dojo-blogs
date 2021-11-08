c35a9ba6479731beba4db44a1c40dd3a263940b0
Khrystyna Bohdan <khrystyna.bohdan@softblues.io>
2021-11-08 17:38:06 +0200

    watch and wathcEffect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,24 +4,39 @@
     <input type="text" v-model="search"/>
     <p>search term -{{search}}</p>
     <div v-for="name in matchNames" :key="name">{{ name }}</div>
+    <button v-on:click="handleClick">stop watching </button>
   </div>
 </template>
 
 <script>
 
-import { computed, ref } from "vue";
+import { computed, ref, watch, watchEffect } from "vue";
 
 export default {
   name: "Home",
   setup() {
     const search = ref('')
     const names = ref(['coco', 'anna', 'ola'])
+
+    const stopwatch= watch(search, () => {
+      console.log ('watch')
+    })
+
+    //watchEffect when component first run
+    const stopwatchEffect = watchEffect(() => {
+      console.log('watchEffect', search.value)
+    })
     //we use computed property to return an updated value based on other values
 
     const matchNames = computed (() => {
       return names.value.filter(name => name.includes(search.value))
     })
-    return {names, search, matchNames};
+
+    const handleClick = () => {
+      stopwatch()
+      stopwatchEffect()
+    }
+    return {names, search, matchNames, handleClick};
   }
 
 };
