1002ce902865dec2565138dddc74349c3a76d334
Khrystyna Bohdan <khrystyna.bohdan@softblues.io>
2021-11-08 17:28:24 +0200

    how we use computed property

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,27 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h1>Refs</h1>
-    <p>{{cocoOne.name}} - {{cocoOne.age}}</p>
-    <button v-on:click="updatecocoOne">update</button>
-    <h2>reactive</h2>
-    <p>{{cocoTwo.name}} - {{cocoTwo.age}}</p>
-    <button v-on:click="updatecocoTwo">update</button>
-
+    <input type="text" v-model="search"/>
+    <p>search term -{{search}}</p>
+    <div v-for="name in matchNames" :key="name">{{ name }}</div>
   </div>
 </template>
 
 <script>
 
-import { ref, reactive } from "vue";
+import { computed, ref } from "vue";
 
 export default {
   name: "Home",
-  //this func(setup) will run before any lifecircle hooks
   setup() {
-    const cocoOne = ref ({name: 'coco', age: 1})
-    const cocoTwo = reactive({name: 'anna', age: 36})
+    const search = ref('')
+    const names = ref(['coco', 'anna', 'ola'])
+    //we use computed property to return an updated value based on other values
 
-    const updatecocoOne = ()=>{
-      cocoOne.value.age = 5
-    }
-
-    const updatecocoTwo= ()=>{
-      cocoTwo.age = 85
-    }
-
-
-    return { cocoOne, updatecocoOne, cocoTwo, updatecocoTwo};
+    const matchNames = computed (() => {
+      return names.value.filter(name => name.includes(search.value))
+    })
+    return {names, search, matchNames};
   }
 
 };
