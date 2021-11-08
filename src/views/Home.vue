4221af24040c28ba3f506a1d6e69542ae4ed44c3
Khrystyna Bohdan <khrystyna.bohdan@softblues.io>
2021-11-08 16:44:18 +0200

    basics of refs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,33 @@
 <template>
   <div class="home">
     home
-    <p>My name is  {{ name }} and age  {{age}}</p>
+    <p ref="p">My name is  {{ name }} and age  {{age}}</p>
     <button v-on:click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
 
+import { ref } from "vue";
+
 export default {
   name: "Home",
   //this func(setup) will run before any lifecircle hooks
   setup() {
-    console.log("setup");
+
+    const p = ref(null);
+
+    console.log(p, p.value);
     let name = "coco";
     let age = 1;
 
     const handleClick = () => {
-      console.log('you clicked me')
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'bohdan'
     }
 
-    return { name, age, handleClick };
+    return { name, age, handleClick, p};
   }
 
 };
